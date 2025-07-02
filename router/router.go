1562edddc5c850eb3ab172b1ab43e66e9ba79dd4
koki <kouki8505@gmail.com>
2025-07-03 00:48:16 +0900

    renew all of endpoints

--- router/router.go
+++ router/router.go
@@ -8,69 +8,78 @@ import (
 	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
-// InitRouter は全てのルーターを初期化する
+// InitRouter は全てのルーターを初期化します
 func InitRouter(r *gin.Engine, h *handler.Handler, ah *handler.AuthHandler, nh *handler.NfcHandler) {
-	// http://localhost:8080/swagger/index.html
+	// Swagger UI: http://localhost:8080/swagger/index.html
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
 	// APIのベースグループ
 	api := r.Group("/api/v1")
 	{
-		// システム
-		// GET /api/v1/ping
-		api.GET("/ping", h.PingHandler) //OK
+		// --- システム ---
+		api.GET("/ping", h.PingHandler)
 
-		// 資産管理
-		//GET /api/v1/assets/:id
-		// id はassetsテーブルのID（主キー）を指定
-		// 資産IDを指定して資産情報を取得するエンドポイント
-		api.GET("/assets/:id", h.GetAssetsByAssetIdHandler) //OK
+		// --- 資産管理 (Assets) ---
+		assets := api.Group("/assets")
+		{
+			// 既存のエンドポイント
+			assets.POST("", h.PostAssetsHandler)            // POST /assets
+			assets.GET("/all", h.GetAssetsAllHandler)       // GET /assets/all (ユーザーの元コード /assetsAll に相当)
+			assets.GET("/:id", h.GetAssetsByAssetIdHandler) // GET /assets/:id
+			assets.PUT("/edit/:id", h.PutAssetsEditHandler) // PUT /assets/edit/:id
 
-		// GET /api/v1/assetsAll
-		// 全資産一覧を取得するエンドポイント
-		api.GET("/assetsAll", h.GetAssetsAllHandler) //OK
+			// 備品マスタ
+			master := assets.Group("/master")
+			{
+				master.GET("/all", h.GetAssetsMasterAllHandler)  // GET /assets/master/all
+				master.GET("/:id", h.GetAssetsMasterByIdHandler) // GET /assets/master/:id
+				master.DELETE("/:id", h.DeleteAssetsHandler)     // DELETE /assets/master/:id
+			}
 
-		// GET /api/v1/assets/master
-		// 全資産マスター一覧を取得するエンドポイント
-		api.GET("/assets/master", h.GetAssetsMasterAllHandler) //OK
+			//【追加】棚卸・点検
+			// idはassetsテーブルの主キー
+			assets.POST("/check/:id", h.PostAssetsCheckHandler) // POST /assets/check/:id
+		}
 
-		//DELETE /api/v1/assets/master/:id
-		// id はassets_masterテーブルのID（主キー）を指定．管理者権限が必要
-		api.DELETE("/assets/master/:id", h.DeleteAssetsHandler) //OK
+		// --- 貸出・返却管理 (Lend/Return) ---
+		lend := api.Group("/lend")
+		{
+			// 既存のエンドポイント
+			lend.GET("/all", h.GetLendsHandler) // GET /lend/all (ユーザーの元コード /lends に相当)
+			// idはassetsテーブルの主キー
+			lend.POST("/:id", h.PostLendHandler) // POST /lend/:id
+			// idはasset_lendsテーブルの主キー
+			lend.POST("/return/:id", h.PostReturnHandler) // POST /lend/return/:id
+			// idはasset_lendsテーブルの主キー
+			lend.GET("/:id", h.GetLendByIdHandler) // GET /lend/:id
 
-		// GET /api/v1/assets/master/:id
-		// id はassets_masterテーブルのID（主キー）を指定．
-		api.GET("/assets/master/:id", h.GetAssetsMasterByIdHandler)
+			//【追加】貸出情報の更新
+			// idはasset_lendsテーブルの主キー
+			lend.PUT("/edit/:id", h.PutLendEditHandler) // PUT /lend/edit/:id
 
-		// POST /api/v1/assets
-		// 新規資産登録のエンドポイント
-		api.POST("/assets", h.PostAssetsHandler) //OK
+			//【追加】利便性向上
+			lend.GET("/overdue", h.GetLendOverdueHandler)                        // GET /lend/overdue
+			lend.GET("/user/:student_id", h.GetLendByStudentIdHandler)           // GET /lend/user/:student_id
+			lend.GET("/history/:student_id", h.GetLendHistoryByStudentIdHandler) // GET /lend/history/:student_id
+		}
 
-		//PUT /api/v1/assets/edit/:id
-		// id はassetsテーブルのID（主キー）を指定
-		// 資産情報を更新するエンドポイント
-		api.PUT("/assets/edit/:id", h.PutAssetsEditHandler) //OK
+		// --- 廃棄管理 (Disposal) ---
+		disposal := api.Group("/disposal")
+		{
+			//【追加】
+			// idはassetsテーブルの主キー
+			disposal.POST("/:id", h.PostDisposalHandler)  // POST /disposal/:id
+			disposal.GET("/all", h.GetDisposalAllHandler) // GET /disposal/all
+			// idはasset_disposalsテーブルの主キー
+			disposal.GET("/:id", h.GetDisposalByIdHandler) // GET /disposal/:id
+		}
 
-		// 貸出管理
-		// POST /api/v1/borrow/:id
-		// id はassetsテーブルのID（主キー）を指定．
-		api.POST("/lend/:id", h.PostLendHandler) //ok
-
-		// POST /api/v1/return/:id
-		// id はasset_lendsテーブルのID（主キー）を指定．
-		api.POST("/lend/return/:id", h.PostReturnHandler) //OK
-
-		// GET /api/v1/borrows
-		// 貸出中一覧を取得するエンドポイント
-		api.GET("/lends", h.GetLendsHandler) //OK
-
-		// GET /api/v1/auth/nfc
-		api.GET("/nfc/read", nh.GetNFC)
-
+		// --- NFC ---
+		nfc := api.Group("/nfc")
+		{
+			nfc.GET("/read", nh.GetNFC) // GET /nfc/read
+		}
 		// 認証関連ルーターの初期化
 		initAuthRouter(api, ah)
-
-		// 他のルーターがあればここに追加
-		// initUserRouter(api)
-		// initProductRouter(api)
 	}
 }
