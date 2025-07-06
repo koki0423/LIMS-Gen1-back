1461856adbcf82eaaebcde113a4be954bb2af630
ResearchPC-koki <al21034@shibaura-it.ac.jp>
2025-07-06 11:50:18 +0900

    Compatible with new handlers

--- router/router.go
+++ router/router.go
@@ -9,7 +9,7 @@ import (
 )
 
 // InitRouter は全てのルーターを初期化します
-func InitRouter(r *gin.Engine, h *handler.Handler, ah *handler.AuthHandler, nh *handler.NfcHandler) {
+func InitRouter(r *gin.Engine, h *handler.Handler, auh *handler.AuthHandler, nh *handler.NfcHandler,ah *handler.AssetHandler,lh *handler.LendHandler, dh *handler.DisposalHandler) {
 	// Swagger UI: http://localhost:8080/swagger/index.html
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
@@ -23,44 +23,44 @@ func InitRouter(r *gin.Engine, h *handler.Handler, ah *handler.AuthHandler, nh *
 		assets := api.Group("/assets")
 		{
 			// 既存のエンドポイント
-			assets.POST("", h.PostAssetsHandler)            // POST /assets
-			assets.GET("/all", h.GetAssetsAllHandler)       // GET /assets/all (ユーザーの元コード /assetsAll に相当)
-			assets.GET("/:id", h.GetAssetsByAssetIdHandler) // GET /assets/:id
-			assets.PUT("/edit/:id", h.PutAssetsEditHandler) // PUT /assets/edit/:id
+			assets.POST("", ah.PostAssetsHandler)            // POST /assets
+			assets.GET("/all", ah.GetAssetsAllHandler)       // GET /assets/all (ユーザーの元コード /assetsAll に相当)
+			assets.GET("/:id", ah.GetAssetsByAssetIdHandler) // GET /assets/:id
+			assets.PUT("/edit/:id", ah.PutAssetsEditHandler) // PUT /assets/edit/:id
 
 			// 備品マスタ
 			master := assets.Group("/master")
 			{
-				master.GET("/all", h.GetAssetsMasterAllHandler)  // GET /assets/master/all
-				master.GET("/:id", h.GetAssetsMasterByIdHandler) // GET /assets/master/:id
-				master.DELETE("/:id", h.DeleteAssetsHandler)     // DELETE /assets/master/:id
+				master.GET("/all", ah.GetAssetsMasterAllHandler)  // GET /assets/master/all
+				master.GET("/:id", ah.GetAssetsMasterByIdHandler) // GET /assets/master/:id
+				master.DELETE("/:id", ah.DeleteAssetsMasterHandler)     // DELETE /assets/master/:id
 			}
 
 			//【追加】棚卸・点検
 			// idはassetsテーブルの主キー
-			assets.POST("/check/:id", h.PostAssetsCheckHandler) // POST /assets/check/:id
+			assets.POST("/check/:id", ah.PostAssetsCheckHandler) // POST /assets/check/:id
 		}
 
 		// --- 貸出・返却管理 (Lend/Return) ---
 		lend := api.Group("/lend")
 		{
 			// 既存のエンドポイント
-			lend.GET("/all", h.GetLendsHandler) // GET /lend/all (ユーザーの元コード /lends に相当)
+			lend.GET("/all", lh.GetLendsHandler) // GET /lend/all (ユーザーの元コード /lends に相当)
 			// idはassetsテーブルの主キー
-			lend.POST("/:id", h.PostLendHandler) // POST /lend/:id
+			lend.POST("/:id", lh.PostLendHandler) // POST /lend/:id
 			// idはasset_lendsテーブルの主キー
-			lend.POST("/return/:id", h.PostReturnHandler) // POST /lend/return/:id
+			lend.POST("/return/:id", lh.PostReturnHandler) // POST /lend/return/:id
 			// idはasset_lendsテーブルの主キー
-			lend.GET("/:id", h.GetLendByIdHandler) // GET /lend/:id
+			lend.GET("/:id", lh.GetLendByIdHandler) // GET /lend/:id
 
 			//【追加】貸出情報の更新
 			// idはasset_lendsテーブルの主キー
-			lend.PUT("/edit/:id", h.PutLendEditHandler) // PUT /lend/edit/:id
+			lend.PUT("/edit/:id", lh.PutLendEditHandler) // PUT /lend/edit/:id
 
 			//【追加】利便性向上
-			lend.GET("/overdue", h.GetLendOverdueHandler)                        // GET /lend/overdue
-			lend.GET("/user/:student_id", h.GetLendByStudentIdHandler)           // GET /lend/user/:student_id
-			lend.GET("/history/:student_id", h.GetLendHistoryByStudentIdHandler) // GET /lend/history/:student_id
+			lend.GET("/overdue", lh.GetLendOverdueHandler)                        // GET /lend/overdue
+			lend.GET("/user/:student_id", lh.GetLendByStudentIdHandler)           // GET /lend/user/:student_id
+			lend.GET("/history/:student_id", lh.GetLendHistoryByStudentIdHandler) // GET /lend/history/:student_id
 		}
 
 		// --- 廃棄管理 (Disposal) ---
@@ -68,10 +68,10 @@ func InitRouter(r *gin.Engine, h *handler.Handler, ah *handler.AuthHandler, nh *
 		{
 			//【追加】
 			// idはassetsテーブルの主キー
-			disposal.POST("/:id", h.PostDisposalHandler)  // POST /disposal/:id
-			disposal.GET("/all", h.GetDisposalAllHandler) // GET /disposal/all
+			disposal.POST("/:id", dh.PostDisposalHandler)  // POST /disposal/:id
+			disposal.GET("/all", dh.GetDisposalAllHandler) // GET /disposal/all
 			// idはasset_disposalsテーブルの主キー
-			disposal.GET("/:id", h.GetDisposalByIdHandler) // GET /disposal/:id
+			disposal.GET("/:id", dh.GetDisposalByIdHandler) // GET /disposal/:id
 		}
 
 		// --- NFC ---
@@ -80,6 +80,6 @@ func InitRouter(r *gin.Engine, h *handler.Handler, ah *handler.AuthHandler, nh *
 			nfc.GET("/read", nh.GetNFC) // GET /nfc/read
 		}
 		// 認証関連ルーターの初期化
-		initAuthRouter(api, ah)
+		initAuthRouter(api, auh)
 	}
 }
