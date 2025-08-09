83fcacfaf1c9a71b3c18dacc8279181e5bc864a5
koki <kouki8505@gmail.com>
2025-08-09 12:36:10 +0900

    delete nfc

--- domain/nfc.go
+++
@@ -1,8 +0,0 @@
-package domain
-
-type StudentCardInfo struct {
-	StudentNumber string `json:"student_number"`
-	// 今後ここにフィールド追加する
-	// Name         string `json:"name,omitempty"`
-	// Expiry       string `json:"expiry,omitempty"`
-}
