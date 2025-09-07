6391c186a321218ab26663bd37e600afd90e7a03
koki <kouki8505@gmail.com>
2025-09-07 16:00:34 +0900

    delete user function

--- domain/user.go
+++
@@ -1,8 +0,0 @@
-package domain
-
-// User はユーザーのデータ構造を表します
-type User struct {
-	ID       uint   `json:"id"`
-	Email    string `json:"email"`
-	Password string `json:"-"` // パスワードはJSONレスポンスに含めないように'-'を指定
-}
